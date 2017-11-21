655381e82e1e0550b8fb18b6d10fde4bffa527de
stal274 <plyushhaj2@gmail.com>
2017-11-21 19:54:57 +0300

    Создание скрипта для всех кнопок в главном меню

---
+++ Basket and stones/Assets/Scripts/MainButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+public class MainButtons : MonoBehaviour
+{
+  
+
+    public void OnMouseUpAsButton()
+    {
+        switch (gameObject.name)
+        {
+            case "Exit":
+                Application.Quit();
+                break;
+
+        }
+    }
+    
+    
+
+
+}
