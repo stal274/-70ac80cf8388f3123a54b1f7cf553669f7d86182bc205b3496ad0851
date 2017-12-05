3c3a3a239aa86887420df4447a7c38b7eb3dc67b
stal274 <plyushhaj2@gmail.com>
2017-12-05 09:18:14 +0300

    Создание класса интерфейса кнопок

---
+++ Basket and stones/Assets/Scripts/GameButton.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class GameButton : MonoBehaviour {
+    public int value;
+    public char action;
+    private int FinalResult;
+ 
+   
+    public void SetGameButton(char action, int value)
+    {
+
+        this.value = value;
+        this.action = action;
+        
+    }
+    public int getResult(int result)
+    {
+        FinalResult = SwitchAction(action, value, result);
+        return FinalResult;
+    }
+    public int SwitchAction(char action, int value, int result)
+    {
+
+        switch (action)
+        {
+            case '*':
+                FinalResult = result * this.value;
+                break;
+            case '+':
+                FinalResult = result + this.value;
+                break;
+            default:
+                break;
+
+        }
+   
+        return FinalResult;
+    }
+    
+  
+}
