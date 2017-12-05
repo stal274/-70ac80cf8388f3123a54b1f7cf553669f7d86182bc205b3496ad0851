60b296895eee5888b4f59c1cc32589604ebddec6
stal274 <plyushhaj2@gmail.com>
2017-12-05 09:15:15 +0300

    Создание класса работы "Компьютера"

---
+++ Basket and stones/Assets/Scripts/AI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class AI : MonoBehaviour
+{
+    private string choise;
+    private int result;
+    private GameButton ButtonRight, ButtonLeft;
+    public int AiStep(GameButton ButtonLeft,GameButton ButtonRight, int res, int final)
+    {
+        
+
+        switch (AiChoise(ButtonLeft.action, ButtonLeft.value, ButtonRight.action, ButtonRight.value, res, final))
+        {
+            case "Left":
+                result = ButtonLeft.getResult(res);
+                break;
+            case "Right":
+                result = ButtonRight.getResult(res);
+                break;
+        }
+        
+        return result;
+    }
+    private string AiChoise(char LeftAction, int LeftValue, char RightAction, int RightValue, int res, int final)
+    {
+        ButtonLeft = new GameButton();
+        ButtonRight = new GameButton();
+        ButtonLeft.SetGameButton(LeftAction, LeftValue);
+        ButtonRight.SetGameButton(RightAction, RightValue);
+        {
+            if (Mathf.Abs(ButtonLeft.getResult(res) - final) < Mathf.Abs(ButtonRight.getResult(res) - final))
+            {
+                choise = "Left";
+            }
+            else if (Mathf.Abs(ButtonLeft.getResult(res) - final) > Mathf.Abs(ButtonRight.getResult(res) - final))
+            {
+                choise = "Right";
+            }
+            else
+            {
+                choise = "Right";
+            }
+            return choise;
+
+        }
+    }
+}
