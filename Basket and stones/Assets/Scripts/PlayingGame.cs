101d74ccbfbde891e3f5ffd26f9476e525fd515f
stal274 <plyushhaj2@gmail.com>
2018-01-21 11:16:17 +0300

    Доработка игрового процесса: появилась задержка между действием игрока и ИИ(наконец-то!)

--- Basket and stones/Assets/Scripts/PlayingGame.cs
+++ Basket and stones/Assets/Scripts/PlayingGame.cs
@@ -10,13 +10,38 @@ public class PlayingGame : MonoBehaviour
     private AI computer;
     private int[] BL = new int[] { 3, 2, 2 };
     private int[] BR = new int[] {2,5,1};
-private char[] Act;
+    private char[] Act;
     public char BLA, BRA;
     private int i, res, fin, BLAI, BRAI;
     
     
    
     private void Start()
+    {
+        ButtonsValueGenerate();
+        ResUpdate();
+    }
+    public void OnClickLeft()
+    {
+        res =  GBL.getResult(res);
+        ResUpdate();
+        res = computer.AiStep(GBL, GBR, res, fin);
+        Invoke("ResUpdate", 2);
+    }
+    public void OnClickRight()
+    {
+        res = GBR.getResult(res);
+        ResUpdate();
+        res = computer.AiStep(GBL, GBR, res, fin);
+        Invoke("ResUpdate", 2);
+
+    }
+   private void ResUpdate()
+    {
+        ResultPanel.GetComponent<Text>().text = System.Convert.ToString(res);
+    }
+    
+  private void ButtonsValueGenerate()
     {
         Act = new char[] { '*', '+' };
         GBL = new GameButton();
@@ -27,33 +52,12 @@ private char[] Act;
         BRAI = Random.Range(0, Act.Length);
         BRA = Act[BRAI];
         BLA = Act[BLAI];
-        ButtonLeft.GetComponentInChildren<Text>().text =BLA+System.Convert.ToString(BL[i]);
-        ButtonRight.GetComponentInChildren<Text>().text =BRA+System.Convert.ToString(BR[i]);
+        ButtonLeft.GetComponentInChildren<Text>().text = BLA + System.Convert.ToString(BL[i]);
+        ButtonRight.GetComponentInChildren<Text>().text = BRA + System.Convert.ToString(BR[i]);
         GBL.SetGameButton(BLA, BL[i]);
         GBR.SetGameButton(BRA, BR[i]);
         res = Random.Range(2, 20);
         fin = Random.Range(20, 40);
-        ResultPanel.GetComponent<Text>().text = System.Convert.ToString(res);
     }
-    public void OnClickLeft()
-    {
-        res =  GBL.getResult(res);
-        ResultPanel.GetComponent<Text>().text = System.Convert.ToString(res);
-        new WaitForSeconds(5);
-        res = computer.AiStep(GBL, GBR, res, fin);
-        
-        ResultPanel.GetComponent<Text>().text = System.Convert.ToString(res);
-        
-    }
-    public void OnClickRight()
-    {
-        res = GBR.getResult(res);
-        ResultPanel.GetComponent<Text>().text = System.Convert.ToString(res);
-        res = computer.AiStep(GBL, GBR, res, fin);
-       
-        ResultPanel.GetComponent<Text>().text = System.Convert.ToString(res);
-
-    }
-    
 
 }
