54077716a2fd357e168f3bf5293fb67c4ef74781
Павел Плющай(stal274) <plyushhaj2@gmail.com>
2018-11-17 09:25:35 +0300

    Корректировка скриптов

--- Basket and stones/Assets/Scripts/MainMenu.cs
+++ Basket and stones/Assets/Scripts/MainMenu.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
@@ -12,7 +14,7 @@ public class MainMenu : MonoBehaviour
         switch (gameObject.name)
         {
             case "StartGame_Button":
-                SceneManager.LoadScene("TestGameScene");
+                SceneManager.LoadScene("LoadingScene");
                 break;
             case "Shop_Button":
                 break;
@@ -33,6 +35,17 @@ public class MainMenu : MonoBehaviour
                 break;
             default:
                 break;
+        }
+    }
+
+    private void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Home) || Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.Menu))
+            {
+                Application.Quit();
+            }
         }
     }
 }
