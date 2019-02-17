d6642a472aa32840dba27bc3f3f6b78cbf1f9fe8
stal274 <plyushhaj2@gmail.com>
2019-02-17 12:36:46 +0300

    Добаление тестовых беков

---
+++ Basket and stones/Assets/Scripts/MainScene/DetectClicks.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectClicks : MonoBehaviour {
+    public GameObject StudyBoard, StudyTrigger;
+    public void OnMouseDown()
+    {
+        StudyBoard.SetActive(false);
+        StudyTrigger.SetActive(false);
+    }
+}
