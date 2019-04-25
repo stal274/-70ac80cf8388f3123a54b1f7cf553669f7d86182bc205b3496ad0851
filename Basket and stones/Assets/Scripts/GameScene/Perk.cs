1cd3cbd4eefa4478403b7aac3fcb005a04dc40db
stal274 <plyushhaj2@gmail.com>
2019-04-25 09:51:59 +0300

    Создание перков:Заморозка,Встряска, Перетасовка действий

--- Basket and stones/Assets/Scripts/GameScene/Perk.cs
+++ Basket and stones/Assets/Scripts/GameScene/Perk.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Diagnostics;
+using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
 {
-    public class Perk
+    public class Perk : PlayingGame
     {
-        private string name;
-        private readonly int cooldown;
-        private Image sprite;
-        private bool IsActive = true;
+        public string name;
+        public readonly int cooldown;
+        public Image sprite;
+        private bool IsActive;
+        public Button Perk0;
 
         public Perk(string name, int cooldown, Image sprite)
         {
@@ -19,24 +23,60 @@ namespace DefaultNamespace
 
         private void PerkActivation()
         {
-            /*switch (name)
+            switch (name)
             {
-                case 
-            }*/
-            //Активация перков через Switch-case
-            IsActive = !IsActive;
+                case "Заморозка":
+                    int i = 0;
+                    i = Random.Range(0, 2);
+                    switch (i)
+
+                    {
+                        case 0:
+                            ButtonLeft.interactable = false;
+                            break;
+                        case 1:
+                            ButtonRight.interactable = false;
+                            break;
+                    }
+
+                    print(name);
+                    break;
+                case "Встряска":
+                    var inti = Random.Range(1, 11);
+                    while (StonesInBasket - inti <= 0)
+                    {
+                        inti = Random.Range(1, 11);
+                    }
+
+                    StonesInBasket -= inti;
+
+                    StonesInBasketUpdate();
+                    break;
+                case "Подмена":
+                    print(name);
+                    ButtonsValueGenerate();
+
+                    break;
+            }
+
+            if (IsActive)
+            {
+                IsActive = !IsActive;
+            }
         }
 
         private void Cooldown(int cooldown)
         {
         }
 
-        public void OnClick()
+        public void PerkOnClick()
         {
-            if (IsActive)
+            if (!IsActive)
             {
                 PerkActivation();
             }
+
+            Perk0.interactable = false;
         }
     }
 }
