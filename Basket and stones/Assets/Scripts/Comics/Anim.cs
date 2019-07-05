c05a5c0e9221e5def7b737e613dc0e55b17fb510
Павел Плющай(stal274) <plyushhaj2@gmail.com>
2019-07-05 09:59:28 +0300

    Прототип анимации слайдов комикса(тестовый, заглушен

--- Basket and stones/Assets/Scripts/Comics/Anim.cs
+++ Basket and stones/Assets/Scripts/Comics/Anim.cs
@@ -1,24 +1,37 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Comics
 {
     public class Anim : MonoBehaviour
     {
-        private String sprite = "Sprite_1";
+        private static int i;
+        private static string sprite = "Sprite_" + i;
 
         public void Awake()
         {
-            GameObject.Find("Sprite_0").GetComponent<UnityEngine.Animation>().Play();
-            Invoke("AnimPlay", 2);
+            GameObject.Find(sprite).GetComponent<UnityEngine.Animation>().Play();
+            Invoke("AnimPlay", 3);
+            Invoke("AnimPlay", 5);
         }
 
-        private void AnimPlay()
+        private static void AnimPlay()
         {
+            i++;
+            sprite = "Sprite_" + i;
             GameObject.Find(sprite).GetComponent<UnityEngine.Animation>()
-                .PlayQueued(
-                    "Comics", QueueMode.CompleteOthers);
+                .Play();
         }
+
+        /*private void Start()
+        {
+            if (PlayerPrefs.GetInt("FirstStart") == 1)
+            {
+                SceneManager.LoadScene("Main menu");
+                PlayerPrefs.SetInt("FirstStart", 0);
+            }
+        }*/
     }
 }
