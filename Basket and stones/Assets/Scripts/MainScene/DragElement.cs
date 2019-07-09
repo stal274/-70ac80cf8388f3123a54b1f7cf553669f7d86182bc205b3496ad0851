cdb272c5dfe06ecebcf0a7dd6e8190e56ef8e3b2
stal274 <plyushhaj2@gmail.com>
2019-07-09 12:54:16 +0300

    Реализация Drag and Drop игровых перков

--- Basket and stones/Assets/Scripts/MainScene/DragElement.cs
+++ Basket and stones/Assets/Scripts/MainScene/DragElement.cs
@@ -1,96 +1,39 @@
+using System;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using UnityEngine.Experimental.UIElements;
-using Image = UnityEngine.UI.Image;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class DragElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] private Image mainImage;
-    private Material mainMaterial;
+    public static GameObject itemBeingDragged;
+    private Vector3 startPosition;
+    public static string name;
 
-    /// <summary>
-    /// Материал, применяемый к элементам на сцене
-    /// </summary>
-    public Material MainMaterial
-    {
-        get { return mainMaterial; }
-        set
-        {
-            if (value != null)
-            {
-                mainMaterial = value;
-                if (mainImage != null)
-                {
-                    mainImage.color = MainMaterial.color;
-                }
-            }
-        }
-    }
-
-    private Transform defaultParentTransform;
-
-    public Transform DefaultParentTransform
-    {
-        get { return defaultParentTransform; }
-        set
-        {
-            if (value != null)
-            {
-                defaultParentTransform = value;
-            }
-        }
-    }
-
-    private Transform dragParentTransform;
-
-    public Transform DragParentTransform
-    {
-        get { return dragParentTransform; }
-        set
-        {
-            if (value != null)
-            {
-                dragParentTransform = value;
-            }
-        }
-    }
-
-    private int siblingindex;
-
-    public int Siblingindex
-    {
-        get { return siblingindex; }
-        set
-        {
-            if (value > 0)
-            {
-                siblingindex = value;
-            }
-        }
-    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        transform.SetParent(DragParentTransform);
+        name = gameObject.name;
+        itemBeingDragged = gameObject;
+        startPosition = transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        GameObject.Find("Drag" + Random.Range(0, 4)).GetComponent<AudioSource>().Play();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position =
-            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        if (Camera.main != null)
+            transform.position =
+                Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        transform.SetParent(DefaultParentTransform);
-        transform.SetSiblingIndex(Siblingindex);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        itemBeingDragged = null;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        if (transform.position != startPosition)
         {
-            hit.collider.gameObject.GetComponent<Renderer>().material = mainMaterial;
+            transform.position = startPosition;
         }
     }
 }
