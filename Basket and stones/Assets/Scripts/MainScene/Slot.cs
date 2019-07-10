87da8272fa8159bdbd70701d2ebc9b2c1183aaf6
stal274 <plyushhaj2@gmail.com>
2019-07-10 11:44:22 +0300

    Класс поведения слота при перетаскивании предмета

---
+++ Basket and stones/Assets/Scripts/MainScene/Slot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    public GameObject item
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item && DragElement.name == transform.name || !item && transform.name == "Backpack slot")
+        {
+            DragElement.itemBeingDragged.transform.SetParent(transform);
+            GameObject.Find("Drop" + Random.Range(0, 6)).GetComponent<AudioSource>().Play();
+        }
+    }
+}
