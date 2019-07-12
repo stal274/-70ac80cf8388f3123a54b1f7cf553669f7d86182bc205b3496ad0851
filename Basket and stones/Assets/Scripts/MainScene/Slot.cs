3e97f9094868c854fe30fedda6c22dc9b7b90b02
stal274 <plyushhaj2@gmail.com>
2019-07-12 11:01:26 +0300

    Добавление функционала заполнения шкалы заполнения слотов рюкзака

--- Basket and stones/Assets/Scripts/MainScene/Slot.cs
+++ Basket and stones/Assets/Scripts/MainScene/Slot.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour, IDropHandler
 {
+    private void Start()
+    {
+    }
+
     public GameObject item
     {
         get
@@ -20,8 +25,35 @@ public class Slot : MonoBehaviour, IDropHandler
     {
         if (!item && DragElement.name == transform.name || !item && transform.name == "Backpack slot")
         {
+            var I = 0;
             DragElement.itemBeingDragged.transform.SetParent(transform);
             GameObject.Find("Drop" + Random.Range(0, 6)).GetComponent<AudioSource>().Play();
+
+            for (var i = 0; i < GameObject.FindGameObjectsWithTag("BackpackSlot").Length; i++)
+            {
+                if (GameObject.FindGameObjectsWithTag("BackpackSlot")[i].GetComponentsInChildren<Image>().Length == 2)
+                {
+                    I++;
+                }
+            }
+
+            var script = FindObjectOfType<BackpackProgressBar>();
+
+
+            switch (I)
+            {
+                case 0:
+                    script.EndFloat = 0f;
+                    break;
+                case 3:
+                    script.EndFloat = 1f;
+                    break;
+                default:
+                    script.EndFloat = I / 3f;
+                    break;
+            }
+
+            script.LoadingBackpack();
         }
     }
 }
