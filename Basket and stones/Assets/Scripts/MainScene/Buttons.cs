182a90188486e1f811e5e7d6be92616f6c8eead0
stal274 <plyushhaj2@gmail.com>
2019-07-13 18:08:14 +0300

    Переход из окна выбора сложности на окно выбора перков

--- Basket and stones/Assets/Scripts/MainScene/Buttons.cs
+++ Basket and stones/Assets/Scripts/MainScene/Buttons.cs
@@ -1,59 +1,59 @@
-using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-
-public class Buttons : MonoBehaviour
+namespace MainScene
 {
-    public GameObject SettingsWindow, MainMenuWindow, DifficultyWindow, StudyBoard, StudyTrigger;
-
-    public GameObject backpackWindow;
-
-
-    public void OnMouseUpAsButton()
+    public class Buttons : MonoBehaviour
     {
-        switch (gameObject.name)
+        public GameObject SettingsWindow, MainMenuWindow, DifficultyWindow, StudyBoard, StudyTrigger;
+
+        public GameObject BackpackWindow;
+        [SerializeField] private GameObject MainMenuWindow1;
+
+
+        public void OnMouseUpAsButton()
         {
-            case "ChooseGame_Button":
-                MainMenuWindow.SetActive(false);
-                DifficultyWindow.SetActive(true);
-                GameObject.Find("SFX_Menu_button").GetComponent<AudioSource>().Play();
-                break;
-            case "Shop_Button":
-                break;
-            case "Settings_Button":
-                SettingsWindow.SetActive(true);
-                MainMenuWindow.SetActive(false);
-                break;
-            case "Achievements_Button":
-                break;
-            case "Study_Button":
-                StudyBoard.SetActive(true);
-                StudyTrigger.SetActive(true);
-                break;
-            case "Exit_Button":
-                Application.Quit();
-                break;
-            case "Exit_Settings_Button":
-                SettingsWindow.SetActive(false);
-                MainMenuWindow.SetActive(true);
-                break;
-            case "StartGame_Button":
-                backpackWindow.SetActive(true);
-                DifficultyWindow.SetActive(false);
-                //GameObject.Find("SFX_New_Game").GetComponent<AudioSource>().Play();
-                //SceneManager.LoadScene("TestGameScene");
-                break;
+            switch (gameObject.name)
+            {
+                case "ChooseGame_Button":
+                    MainMenuWindow.SetActive(false);
+                    DifficultyWindow.SetActive(true);
+                    GameObject.Find("SFX_Menu_button").GetComponent<AudioSource>().Play();
+                    break;
+                case "Shop_Button":
+                    break;
+                case "Settings_Button":
+                    SettingsWindow.SetActive(true);
+                    MainMenuWindow.SetActive(false);
+                    break;
+                case "Achievements_Button":
+                    break;
+                case "Study_Button":
+                    StudyBoard.SetActive(true);
+                    StudyTrigger.SetActive(true);
+                    break;
+                case "Exit_Button":
+                    Application.Quit();
+                    break;
+                case "Exit_Settings_Button":
+                    SettingsWindow.SetActive(false);
+                    MainMenuWindow.SetActive(true);
+                    break;
+                case "StartGame_Button":
+                    BackpackWindow.transform.position = MainMenuWindow1.transform.position;
+                    BackpackProgressBar.CheckBackPack();
+                    DifficultyWindow.SetActive(false);
+                    break;
+            }
         }
-    }
 
-    private void OnMouseDown()
-    {
-        transform.localScale = new Vector3(0.95f, 0.95f, 1f);
-    }
+        private void OnMouseDown()
+        {
+            transform.localScale = new Vector3(0.95f, 0.95f, 1f);
+        }
 
-    private void OnMouseUp()
-    {
-        transform.localScale = new Vector3(1f, 1f, 1f);
+        private void OnMouseUp()
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
