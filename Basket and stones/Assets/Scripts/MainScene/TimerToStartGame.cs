bec37b062cd620201db99603538a672110df44d9
stal274 <plyushhaj2@gmail.com>
2019-07-13 18:01:54 +0300

    Класс таймера По окончании запускает игру

---
+++ Basket and stones/Assets/Scripts/MainScene/TimerToStartGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MainScene
+{
+    public class TimerToStartGame : MonoBehaviour
+    {
+        [SerializeField] private GameObject Timer;
+        [SerializeField] private Text Text;
+
+        public static bool StartTick { private get; set; }
+
+        private IEnumerator Tick()
+        {
+            for (var i = 5; i >= 0; i--)
+            {
+                Text.text = "0:0" + i;
+                if (i == 0)
+                {
+                    new WaitForSeconds(0.5f);
+                    SceneManager.LoadScene("TestGameScene");
+                }
+
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        // Update is called once per frame
+        private void Start()
+        {
+            Timer = GameObject.Find("Timer");
+            Text = Timer.GetComponent<Text>();
+        }
+
+        public void StartTicker()
+        {
+            if (StartTick)
+            {
+                StartCoroutine(Tick());
+            }
+            else
+            {
+                Text.text = "0:05";
+                StopAllCoroutines();
+            }
+        }
+    }
+}
