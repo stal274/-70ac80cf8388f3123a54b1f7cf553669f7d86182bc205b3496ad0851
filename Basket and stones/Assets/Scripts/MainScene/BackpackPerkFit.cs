976faa9ed1df24123a845e09ec84da7a549dc68e
stal274 <plyushhaj2@gmail.com>
2019-07-13 18:01:10 +0300

    Класс, загружающий перки в рюкзак

---
+++ Basket and stones/Assets/Scripts/MainScene/BackpackPerkFit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainScene
+{
+    public class BackpackPerkFit : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] BackpackSlots;
+
+        private void Start()
+        {
+            for (var i = 0; i < BackpackSlots.Length; i++)
+            {
+                if (GameObject.FindGameObjectsWithTag("BackpackSlot")[i].GetComponentsInChildren<Image>().Length == 1)
+                {
+                    if (!((IList) GameObject.FindGameObjectsWithTag("Perk"))
+                            .Contains(GameObject.Find(PlayerPrefs.GetString("BackpackSlot" + i))) ||
+                        PlayerPrefs.GetString("BackpackSlot" + i) == null) continue;
+                    DragElement.itemBeingDragged = GameObject.Find(PlayerPrefs.GetString("BackpackSlot" + i));
+                    DragElement.itemBeingDragged.transform.SetParent(BackpackSlots[i].transform);
+                }
+            }
+        }
+    }
+}
