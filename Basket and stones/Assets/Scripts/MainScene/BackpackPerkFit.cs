6837c6d157cbd9ede0d6648531ff9fb5b7bc764c
stal274 <plyushhaj2@gmail.com>
2019-07-16 11:07:05 +0300

    Улучшена читабельность кода, исправлена проблема заполнения рюкзака на платформе Андроид

--- Basket and stones/Assets/Scripts/MainScene/BackpackPerkFit.cs
+++ Basket and stones/Assets/Scripts/MainScene/BackpackPerkFit.cs
@@ -6,20 +6,18 @@ namespace MainScene
 {
     public class BackpackPerkFit : MonoBehaviour
     {
-        [SerializeField] private GameObject[] BackpackSlots;
-
         private void Start()
         {
-            for (var i = 0; i < BackpackSlots.Length; i++)
+            for (var i = 0; i < GameObject.FindGameObjectsWithTag("BackpackSlot").Length; i++)
             {
-                if (GameObject.FindGameObjectsWithTag("BackpackSlot")[i].GetComponentsInChildren<Image>().Length == 1)
-                {
-                    if (!((IList) GameObject.FindGameObjectsWithTag("Perk"))
-                            .Contains(GameObject.Find(PlayerPrefs.GetString("BackpackSlot" + i))) ||
-                        PlayerPrefs.GetString("BackpackSlot" + i) == null) continue;
-                    DragElement.itemBeingDragged = GameObject.Find(PlayerPrefs.GetString("BackpackSlot" + i));
-                    DragElement.itemBeingDragged.transform.SetParent(BackpackSlots[i].transform);
-                }
+                if (GameObject.FindGameObjectsWithTag("BackpackSlot")[i].GetComponentsInChildren<Image>().Length !=
+                    1) continue;
+                if (!((IList) GameObject.FindGameObjectsWithTag("Perk"))
+                        .Contains(GameObject.Find(PlayerPrefs.GetString("BackpackSlot" + i))) ||
+                    PlayerPrefs.GetString("BackpackSlot" + i) == null) continue;
+                DragElement.itemBeingDragged = GameObject.Find(PlayerPrefs.GetString("BackpackSlot" + i));
+                DragElement.itemBeingDragged.transform.SetParent(GameObject.FindGameObjectsWithTag("BackpackSlot")[i]
+                    .transform, false);
             }
         }
     }
