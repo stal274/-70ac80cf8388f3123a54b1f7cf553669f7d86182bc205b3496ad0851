a187e443c382422318f359720b75157e8a33b64c
stal274 <plyushhaj2@gmail.com>
2019-07-16 11:08:53 +0300

    Добавлена фича: при дропе из рюкзака в пустое пространство перк возврашается в свой слот в каталоге

--- Basket and stones/Assets/Scripts/MainScene/DragElement.cs
+++ Basket and stones/Assets/Scripts/MainScene/DragElement.cs
@@ -1,5 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace MainScene
 {
@@ -8,13 +11,27 @@ namespace MainScene
         public static GameObject itemBeingDragged;
         private Vector3 startPosition;
         public static string name;
+        [SerializeField] private Transform FirstParent;
 
+        [SerializeField] private bool parentIsBackpack;
+
+        private void Start()
+        {
+            for (var i = 0; i < GameObject.FindGameObjectsWithTag("PerkSlot").Length; i++)
+            {
+                if (GameObject.FindGameObjectsWithTag("PerkSlot")[i].name != gameObject.name) continue;
+                FirstParent = GameObject.FindGameObjectsWithTag("PerkSlot")[i].transform;
+                break;
+            }
+        }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
             name = gameObject.name;
             itemBeingDragged = gameObject;
             startPosition = transform.position;
+            parentIsBackpack = Math.Abs(itemBeingDragged.GetComponentInParent<GridLayoutGroup>().cellSize.x - 72) <
+                               0.01f;
             GetComponent<CanvasGroup>().blocksRaycasts = false;
             GameObject.Find("Drag" + Random.Range(0, 4)).GetComponent<AudioSource>().Play();
         }
@@ -31,9 +48,13 @@ namespace MainScene
             itemBeingDragged = null;
             GetComponent<CanvasGroup>().blocksRaycasts = true;
             BackpackProgressBar.CheckBackPack();
-            if (transform.position != startPosition)
+            if (transform.position == startPosition) return;
+            transform.position = parentIsBackpack
+                ? FirstParent.position
+                : startPosition;
+            if (parentIsBackpack)
             {
-                transform.position = startPosition;
+                gameObject.transform.SetParent(FirstParent);
             }
         }
     }
