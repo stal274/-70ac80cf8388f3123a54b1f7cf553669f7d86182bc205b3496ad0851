e13849d1b31d1a8164d65df0f529fcca6d4f93c6
stal274 <plyushhaj2@gmail.com>
2019-07-31 10:23:46 +0300

    Класс выхода из описания перка

---
+++ Basket and stones/Assets/Scripts/Prefabs/ClickToExit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickToExit : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private GameObject[] objectsToVisible;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        gameObject.SetActive(false);
+        foreach (var VARIABLE in objectsToVisible)
+        {
+            VARIABLE.SetActive(true);
+        }
+    }
+}
