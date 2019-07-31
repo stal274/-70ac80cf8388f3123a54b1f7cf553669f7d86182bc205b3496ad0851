ab6b8f3ec527e7d6b712dae8d10b5cd6f9cd451a
stal274 <plyushhaj2@gmail.com>
2019-07-31 10:19:04 +0300

    Класс включения описания перка после нажатия на иконку перка

---
+++ Basket and stones/Assets/Scripts/Prefabs/HoldButton.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Prefabs
+{
+    public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    {
+        private bool flag;
+        private int timer;
+        [SerializeField] private Text descriptionText, actionText;
+        [SerializeField] private GameObject panelOfDescription;
+        [SerializeField] private GameObject[] objectsToVisible;
+
+
+        private IEnumerator OnHold()
+        {
+            yield return new WaitForSeconds(1f);
+
+            DescriptionActivation();
+        }
+
+        private void DescriptionActivation()
+        {
+            descriptionText.text = gameObject.GetComponentsInChildren<Text>()[0].text;
+            actionText.text = gameObject.GetComponentsInChildren<Text>()[1].text;
+            panelOfDescription.SetActive(true);
+
+
+            foreach (var VARIABLE in objectsToVisible)
+            {
+                VARIABLE.SetActive(false);
+            }
+        }
+
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            StartCoroutine(OnHold());
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            StopAllCoroutines();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            StopAllCoroutines();
+        }
+    }
+}
