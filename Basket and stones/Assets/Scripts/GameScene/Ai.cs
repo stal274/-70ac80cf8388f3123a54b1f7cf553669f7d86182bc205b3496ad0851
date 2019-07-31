9802cdc0bb50582307f0bf3f0e57f58bbd623190
stal274 <plyushhaj2@gmail.com>
2019-07-31 09:50:55 +0300

    Рефакторинг кода ИИ

--- Basket and stones/Assets/Scripts/GameScene/Ai.cs
+++ Basket and stones/Assets/Scripts/GameScene/Ai.cs
@@ -1,54 +1,62 @@
-using System;
+using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
-using Button = UnityEngine.UI.Button;
+using UnityEngine.UI;
 
 namespace GameScene
 {
     public class Ai : MonoBehaviour
     {
-        private string _choice;
-
-        public Button ButtonLeft, ButtonRight;
-        private PlayingGame Pg;
+        [NotNull] private string choice;
+        [SerializeField] private GameButton[] gameButton;
+        public Button[] buttonsAi;
+        private Basket Basket;
 
         private void Start()
         {
-            Pg = FindObjectOfType<PlayingGame>();
+            Basket = FindObjectOfType<Basket>();
+        }
+
+        public void AiStep()
+        {
+            var stopgame = FindObjectOfType<PlayingGame>().StopGame;
+            if (stopgame) return;
+
+            StartCoroutine(AiChoice());
         }
 
         public int AiStep()
         {
-            AiChoice();
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+
+            choice =
+                Mathf.Abs(Basket.Calculate(gameButton[0].Action, gameButton[0].Value, true) - Basket.StonesToWin) <=
+                Mathf.Abs(Basket.Calculate(gameButton[1].Action, gameButton[1].Value, true) - Basket.StonesToWin)
+                    ? "Left"
+                    : "Right";
             // ReSharper disable once ConvertIfStatementToSwitchStatement
-            if (_choice == "Left" && ButtonLeft.IsInteractable() || _choice == "Right" && !ButtonRight.IsInteractable())
+            if (choice == "Left" && buttonsAi[0].IsInteractable() ||
+                choice == "Right" && !buttonsAi[1].IsInteractable())
             {
-                Pg.StonesInBasket = Pg.GameButtonLeft.getResult(Pg.StonesInBasket);
+                Basket.Calculate(gameButton[0]);
             }
-            else if (_choice == "Right" && ButtonRight.IsInteractable() ||
-                     _choice == "Left" && !ButtonLeft.IsInteractable())
+            else if (choice == "Right" && buttonsAi[1].IsInteractable() ||
+                     choice == "Left" && !buttonsAi[0].IsInteractable())
             {
-                Pg.StonesInBasket = Pg.GameButtonRight.getResult(Pg.StonesInBasket);
+                Basket.Calculate(gameButton[1]);
             }
 
+            // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
 
-            return Pg.StonesInBasket;
-        }
-
-
-        private void AiChoice()
-        {
+            foreach (var variable in gameButton)
             {
-                if (Mathf.Abs(Pg.GameButtonLeft.getResult(Pg.StonesInBasket) - Pg.WinningNumberStones) <=
-                    Mathf.Abs(Pg.GameButtonRight.getResult(Pg.StonesInBasket) - Pg.WinningNumberStones))
-                {
-                    _choice = "Left";
-                }
-                else if (Mathf.Abs(Pg.GameButtonLeft.getResult(Pg.StonesInBasket) - Pg.WinningNumberStones) >=
-                         Mathf.Abs(Pg.GameButtonRight.getResult(Pg.StonesInBasket) - Pg.WinningNumberStones))
-                {
-                    _choice = "Right";
-                }
+                // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                variable.GetComponent<Button>().interactable = true;
             }
+
+            GameObject.Find("SFX_Tern_button_" + Random.Range(3, 5)).GetComponent<AudioSource>().Play();
+            var pg = FindObjectOfType<PlayingGame>();
+            pg.WhoseTurn = "Human";
         }
     }
 }
