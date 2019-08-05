b888e0c09595313990e4103dbb75f621671a3ebb
stal274 <plyushhaj2@gmail.com>
2019-08-05 15:53:09 +0300

    Добавление анимации изменения камней в корзинке

--- Basket and stones/Assets/Scripts/GameScene/Basket.cs
+++ Basket and stones/Assets/Scripts/GameScene/Basket.cs
@@ -60,10 +60,31 @@ namespace GameScene
             Calculate(button.Action, button.Value, false);
         }
 
-        private void StonesInBasketGenerate(int i)
+        public IEnumerator StonesInBasketGenerate()
         {
-            currentAmountOfStones = i;
-            currentAmountOfStonesPanel.text = Convert.ToString(currentAmountOfStones);
+            for (var j = int.Parse(currentAmountOfStonesPanel.text);;
+            )
+            {
+                yield return new WaitForSeconds((float) 1 / (5 * Math.Abs(j - CurrentAmountOfStones)));
+                if (j > CurrentAmountOfStones) j--;
+                if (j < CurrentAmountOfStones) j++;
+                StartCoroutine(BasketAnimation(1f, 1.2f));
+                currentAmountOfStonesPanel.text = Convert.ToString(j);
+
+
+                if (j == CurrentAmountOfStones) break;
+            }
+
+            StartCoroutine(BasketAnimation(1.2f, 1f));
+        }
+
+        private IEnumerator BasketAnimation(float startFloat, float endFloat)
+        {
+            for (var i = startFloat; Math.Abs(i - endFloat) > 0.01f; i += startFloat < endFloat ? 0.05f : -0.05f)
+            {
+                yield return new WaitForSeconds(1 / 20f);
+                currentAmountOfStonesPanel.transform.localScale = new Vector3(i, i);
+            }
         }
     }
 }
