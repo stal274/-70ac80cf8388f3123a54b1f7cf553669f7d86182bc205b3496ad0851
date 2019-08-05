10a6e5af313560da903b50ac349dc838ff2f5846
stal274 <plyushhaj2@gmail.com>
2019-08-05 15:43:08 +0300

    Улучшение класса смены окон сцены

--- Basket and stones/Assets/Scripts/Prefabs/ClickToExit.cs
+++ Basket and stones/Assets/Scripts/Prefabs/ClickToExit.cs
@@ -1,13 +1,33 @@
-﻿using UnityEngine;
+﻿using GameScene;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ClickToExit : MonoBehaviour, IPointerDownHandler
+public class ClickToExit : MonoBehaviour, IPointerDownHandler, IPhoneButtons
 {
     [SerializeField] private GameObject[] objectsToVisible;
 
+    private void Update()
+    {
+        if (!gameObject.activeSelf) return;
+        if (Application.platform != RuntimePlatform.Android) return;
+        // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+        HardwareButtons(KeyCode.Escape);
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
-        gameObject.SetActive(false);
+        Exit();
+    }
+
+    public void HardwareButtons(KeyCode escapeButton)
+    {
+        if (!Input.GetKeyDown(escapeButton)) return;
+        Exit();
+    }
+
+    private void Exit()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
         foreach (var VARIABLE in objectsToVisible)
         {
             VARIABLE.SetActive(true);
