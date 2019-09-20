ff066df8514d4eba6393ead542dd343e8ddb0fe4
stal274 <plyushhaj2@gmail.com>
2019-09-20 11:52:37 +0300

    Попытка в юнит- тесты(спойлер удачная)

---
+++ Basket and stones/Assets/Scripts/Tests/GameScene/Basket/BasketCalculating.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using GameScene;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BasketCalculating
+    {
+        private Basket basket;
+        // A Test behaves as an ordinary method
+        [SetUp]
+        public void Setup()
+        {
+            basket = new Basket();
+        }
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(basket);
+        }
+        [UnityTest]
+        public IEnumerator BasketCalculateSumm()
+        {
+
+            int value = 10;
+            char action = '+';
+            int stonesBeforeTest = 5;
+            basket.CurrentAmountOfStones = stonesBeforeTest;
+            int stonesAfterTest = basket.Calculate(action, value, true);
+            Assert.AreEqual(stonesBeforeTest + 10, stonesAfterTest);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator BasketCalculateProd()
+        {
+            int value = 10;
+            char action = '*';
+            int stonesBeforeTest = 5;
+            basket.CurrentAmountOfStones = stonesBeforeTest;
+            int stonesAfterTest = basket.Calculate(action, value, true);
+            Assert.AreEqual(stonesBeforeTest * 10, stonesAfterTest);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator BasketCalculateDiff()
+        {
+
+            int value = 10;
+            char action = '-';
+            int stonesBeforeTest = 5;
+            basket.CurrentAmountOfStones = stonesBeforeTest;
+            int stonesAfterTest = basket.Calculate(action, value, true);
+            Assert.AreEqual(stonesBeforeTest - 10, stonesAfterTest);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator BasketCalculateDiv()
+        {
+
+            int value = 10;
+            char action = '/';
+            int stonesBeforeTest = 5;
+            basket.CurrentAmountOfStones = stonesBeforeTest;
+            int stonesAfterTest = basket.Calculate(action, value, true);
+            Assert.AreEqual(stonesBeforeTest / 10, stonesAfterTest);
+            yield return null;
+        }
+    }
+}
