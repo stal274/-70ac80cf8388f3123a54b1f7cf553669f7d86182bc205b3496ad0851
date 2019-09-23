fe88b6dd6de7e6a43b07e2400391c94560b60a2c
stal274 <plyushhaj2@gmail.com>
2019-09-23 13:40:08 +0300

    Код по работе с кошельком

---
+++ Basket and stones/Assets/Scripts/Prefabs/FireCoinsIndicator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FireCoinsIndicator : MonoBehaviour
+{
+    private Text fireCoinsText;
+    public static FireCoinsIndicator fireCoins;
+    private void Awake()
+    {
+        if (fireCoins != null)
+        {
+            return;
+        }
+        fireCoins = this;
+        fireCoinsText = GameObject.Find("FireCoin_Value").GetComponent<Text>();
+    }
+    private void Start()
+    {
+        CoinsValueUpdate();   
+    }
+    public void CoinsValueUpdate()
+    {
+        fireCoinsText.text = System.Convert.ToString(Player.Wallet.wallet.FireCoins);
+    }
+}
