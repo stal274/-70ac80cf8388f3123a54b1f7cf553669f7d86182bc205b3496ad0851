ec9b68d378bb2d5a60930d3a62101e05551424c3
stal274 <plyushhaj2@gmail.com>
2019-10-15 12:40:55 +0300

    Переименовывание переменных

--- Basket and stones/Assets/Scripts/GameScene/SafeDepositOfButtonActions.cs
+++ Basket and stones/Assets/Scripts/GameScene/SafeDepositOfButtonActions.cs
@@ -4,39 +4,41 @@ namespace GameScene
 {
     public class SafeDepositOfButtonActions : MonoBehaviour
     {
-        private readonly int[,] Values =
+        private readonly int[,] _values =
             {{3, 2, 2, 3, 4, 6, 5, 3, 2, 6, 4, 3, 3, 5, 4}, {2, 5, 7, 4, 3, 5, 4, 6, 4, 7, 3, 2, 7, 3, 6}};
 
-        private readonly char[,] Actions =
+        private readonly char[,] _actions =
         {
             {'+', '*', '+', '+', '-', '+', '-', '+', '*', '+', '-', '+', '-', '+', '-'},
             {'-', '-', '-', '-', '+', '-', '+', '-', '-', '-', '+', '-', '+', '-', '+'}
         };
-        private int index;
 
-        public static SafeDepositOfButtonActions bank;
+        private int _index;
+
+        public static SafeDepositOfButtonActions Bank;
 
 
         private void Awake()
         {
-            if (bank != null)
+            if (Bank != null)
             {
                 Debug.LogWarning("Something Wrong!");
                 return;
             }
-            bank = this;
+
+            Bank = this;
         }
+
         public void GenerateIndex()
         {
             var button = FindObjectsOfType<GameButton>();
-            index = Random.Range(0, Values.GetLength(1));
+            _index = Random.Range(0, _values.GetLength(1));
             foreach (var variable in button)
             {
-                variable.Action = Actions[variable.NumberOfGameButton, index];
-                variable.Value = Values[variable.NumberOfGameButton, index];
-                variable.ButtonsValueGenerate();
+                variable.Action = _actions[variable.NumberOfGameButton, _index];
+                variable.Value = _values[variable.NumberOfGameButton, _index];
+                variable.ButtonsActionAndValueEditing();
             }
-           
         }
     }
 }
