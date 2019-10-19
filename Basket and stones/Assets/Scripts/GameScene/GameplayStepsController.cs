b4d724f5d1280ffd810639a2bfa5a004c2a9e816
stal274 <plyushhaj2@gmail.com>
2019-10-19 08:48:07 +0300

    Перенос логики кнопок в другой класс, согласно правилу единой ответственности

--- Basket and stones/Assets/Scripts/GameScene/GameplayStepsController.cs
+++ Basket and stones/Assets/Scripts/GameScene/GameplayStepsController.cs
@@ -1,4 +1,4 @@
-﻿using MainScene;
+using MainScene;
 using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -71,24 +71,6 @@ namespace GameScene
             }
 
             /*stones.GetComponent<Stones>();*/
-        }
-
-
-        public void Click(GameObject obj)
-        {
-            GameObject.Find("SFX_New_Game").GetComponent<AudioSource>().Play();
-            switch (obj.name)
-            {
-                case "Retry_Button":
-                    SceneManager.LoadScene("TestGameScene");
-                    break;
-                case "MainMenu_Button":
-                    SceneManager.LoadScene("Main menu");
-                    break;
-                default:
-                    Debug.LogWarning("Something wrong!");
-                    break;
-            }
         }
 
 
